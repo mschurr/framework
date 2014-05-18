885a987fc582949a0c5754ec229e48dcf5e3de02
Matthew Schurr <mschurr@rice.edu>
2014-05-18 13:55:50 -0700

    * Updated command-line handler.

--- src/framework/cli.php
+++ src/framework/cli.php
@@ -1,5 +1,11 @@
 <?php
 
+class CLIExitStatus
+{
+	const Success = 0;
+	const Failure = 1;
+}
+
 class CLIApplication
 {
 	/**
@@ -18,9 +24,8 @@ class CLIApplication
 	/**
 	 * Displays the usage error message.
 	 */
-	protected /*void*/ function clierror()
+	protected /*void*/ function clierror(/*array<string>*/ $argv)
 	{
-		global $argv;
 		printf("Usage: php ".$argv[0]." <command> <...args>\r\n");
 		printf("\r\nRecognized Commands:\r\n");
 		foreach($this->routes as $cmd => $targ)
@@ -31,7 +36,7 @@ class CLIApplication
 	/**
 	 * Insantiates the object.
 	 */
-	protected function __construct()
+	protected /*void*/ function __construct()
 	{
 		$this->routes = array();
 	}
@@ -46,56 +51,74 @@ class CLIApplication
 	}
 
 	/**
-	 * Runs a CLI application.
+	 * Dispatches an incoming command-line request and runs the command. Returns status.
 	 */
-	protected /*int*/ function run()
+	protected /*int*/ function dispatch()
 	{
 		global $argv, $argc;
 
-		if($argc < 1) {
-			$this->clierror();
-			return -1;
-		}
-
+		// Resolve Command
 		if($argc < 2) {
-			$this->clierror();
-			return -1;
+			$this->clierror($argv);
+			return CLIExitStatus::Failure;
 		}
 
 		$command = $argv[1];
 
+		// Resolve Target
 		if(!isset($this->routes[$command])) {
-			$this->clierror();
-			return -1;
+			$this->clierror($argv);
+			return CLIExitStatus::Failure;
 		}
 
 		$target = $this->routes[$command];
 
+		// Handle Class Targets
 		if(is_string($target)) {
 			if(!class_exists($target)) {
-				printf("Invalid command target for '".$command."'. \r\n");
-				return -1;
+				fprintf(STDOUT, "Invalid command target for '".$command."' (not found).\r\n");
+				return CLIExitStatus::Failure;
 			}
 
 			$handler = new $target();
 
 			if(!is_subclass_of($handler,'CLIApplicationController')) {
-				printf("Invalid command target for '".$command."'. \r\n");
-				return -1;
+				fprintf(STDOUT, "Invalid command target for '".$command."' (must inherit from CLIApplicationController).\r\n");
+				return CLIExitStatus::Failure;
 			}
 
-			return $handler->run(array_slice($argv, 1));
+			$status = $handler->main(array_slice($argv, 1));
+			return ($status === null) ? CLIExitStatus::Success : $status;
 		}
 
+		// Handle Closure Targets
 		if($target instanceof Closure) {
-			return $target(array_slice($argv, 1));
+			$status = $target(array_slice($argv, 1));
+			return ($status === null) ? CLIExitStatus::Success : $status;
 		}
 
-		$this->clierror();
-		return -1;
+		// Handle Unknown Target
+		fprintf(STDOUT, "Unable to resolve target for command \"%s\"\r\n", $command);
+		return CLIExitStatus::Failure;
+	}
+
+	/**
+	 * Runs a CLI application.
+	 */
+	protected /*void*/ function run()
+	{
+		$status = $this->dispatch();
+
+		Config::_save();
+
+		exit($status);	
 	}
 }
 
+/**
+ * An abstract controller class for command-line applications.
+ * Command-line controllers should inherit from this class and override main().
+ */
 abstract class CLIApplicationController
 {
 	protected $_users;
@@ -105,53 +128,53 @@ abstract class CLIApplicationController
 	protected $_stdin;
 	protected $_stdout;
 
-	public function __construct()
-	{
+	/**
+	 * Runs the application.
+	 * $args contains the command-line arguments, beginning with the routed command.
+	 * Returns the status (0 = successful) or an error code.
+	 */
+	public abstract /*int*/ function main(/*array<string>*/ $args);
 
-	}
-
-	public abstract function run($args);
-
-	public function __isset(/*scalar*/ $key)
+	public /*boolean*/ function __isset(/*scalar*/ $key)
 	{
 		return method_exists($this, 'get'.ucfirst($key));
 	}
 
-	public function __get($key)
+	public /*mixed*/ function __get(/*scalar*/ $key)
 	{
 		return $this->{'get'.ucfirst($key)}();
 	}
 
-	public function getStderr()
+	public /*int*/ function getStderr()
 	{
 		return STDERR;
 	}
 
-	public function getStdin()
+	public /*int*/ function getStdin()
 	{
 		return STDIN;
 	}
 
-	public function getStdout()
+	public /*int*/ function getStdout()
 	{
 		return STDOUT;
 	}
 
-	public function getDatabase()
+	public /*Database*/ function getDatabase()
 	{
 		if(!$this->_database)
 			$this->_database = App::getDatabase();
 		return $this->_database;
 	}
 
-	public function getUsers()
+	public /*User_Service_Provider*/ function getUsers()
 	{
 		if(!$this->_users)
 			$this->_users = App::getUserService();
 		return $this->_users;
 	}
 
-	public function getGroups()
+	public /*Group_Service_Provider*/ function getGroups()
 	{
 		if(!$this->_groups)
 			$this->_groups = App::getGroupService();
