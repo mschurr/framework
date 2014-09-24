8540ef1e12b955f99ee3aa914964e3dc2b281931
Matthew Schurr <mschurr@rice.edu>
2014-09-24 18:04:50 -0500

    * Fixed MySQL drivers and database drivers for better error handling.

--- src/framework/database.php
+++ src/framework/database.php
@@ -1,15 +1,15 @@
 <?php
 /*
 	Database Abstraction Layer
-	
+
 	--------------------------------------
-	
+
 	Supported Drivers:
 		mysql
-	
+
 	Usage:
 		$db = new Database($driver, $host, $port, $user, $pass, $name);
-		
+
 		NOTE: By default, these values are filled with the following configuration values (Config::get(value)):
 			database.driver
 			database.host
@@ -17,36 +17,36 @@
 			database.user
 			database.pass
 			database.name
-		
+
 		You can also retrieve a pointer to the web application database at any time using:
 		$db =& App::DB();
-		
+
 		NOTE: This ensures that only one instance is created, and that it is only created when it will be used.
-		
+
 		After getting a reference to a database object, you can execute commands to the driver.
-		
+
 		$query = $db->query("SELECT * FROM `my_table`;");
-		
+
 		You should escape parameters in non-prepared statements to prevent injection attacks:
 		$query = $db->query("INSERT INTO `my_table` (`name`) VALUES ('".$db->escape($string)."');");
-		
+
 		You can also use prepared statements as follows:
 		$statement = $db->prepare("INSERT INTO `my_table` (`name`) VALUES (?);");
 		$query = $statement->execute(array('my name'));
-		
+
 		If you wish, you may also name the parameters:
 		$statement = $db->prepare("INSERT INTO `my_table` (`name`) VALUES (:name);");
 		$query = $statement->execute(array(':name' => 'my name'));
 
 		NOTE: All ->prepare operations return an instance of DB_PreparedStatement.
 		NOTE: All ->query or ->execute operations return an instance of DB_Result.
-		
+
 		You may also wish to use transactions:
-		
+
 		$db->beginTransaction();
-		
+
 		... queries or prepared statements ...
-		
+
 		if(!$db->commit())
 			$db->rollback();
 
@@ -57,7 +57,7 @@
 		});
 
 		The database will automatically return to autocommit mode after a transaction ends.
-			
+
 		All query results have the following properties (DB_Result):
 			$result->affected			The number of rows affected.
 			$result->insertId			The insert id (unique key), if applicable.
@@ -69,33 +69,62 @@
 			$result->driver				A pointer to the database driver.
 			$result->time				The time (in milliseconds) that the query took to execute.
 			$result->error				The error that occured executing the query (if applicable).
-			
+
 		You can also use the following:
 			len($result)				Returns the size of the result set.
 			foreach($result as $row){}	Iterate through the result set ($row is a column associative array).
 			$result['key'] 				Equivalent to $result->row['key'].
-			
+
 		Important:
 			All queries that are not part of a transaction will throw a DatabaseException on failure.
-			
+
 */
 
-class DatabaseException extends Exception {}
+class DatabaseException extends Exception {
+	protected /*array<string>|array<string, string>*/ $params = [];
+	protected /*string*/ $query = '';
+
+	public /*void*/ function __construct($query, $error, $params) {
+		parent::__construct($error);
+		$this->query = $query;
+		$this->params = $params;
+	}
+
+	public /*string*/ function getError() {
+		return $this->getMessage();
+	}
+
+	public /*string*/ function getQuery() {
+		return $this->query;
+	}
+
+	public /*array<string>|array<string, string>*/ function getParams() {
+		return $this->params;
+	}
+
+	public /*string*/ function getExplanation() {
+		return "A database query failed: ".$this->query." : ".json_encode($this->params)." : ".$this->getMessage();
+	}
+
+	public /*string*/ function __toString() {
+		return $this->getExplanation();
+	}
+}
 
 class Database
 {
 	protected $driver;
 	protected $driver_name;
-	
+
 	public function __construct($driver=null, $host=null, $port=null, $user=null, $pass=null, $name=null)
 	{
 		$this->driver_name = $this->pick('database.driver', 'mysql', $driver);
 		$class = 'DB_Driver_'.$this->driver_name;
-		
+
 		if(!class_exists($class)) {
 			import('db-'.$this->driver_name);
 		}
-		
+
 		$this->driver = new $class(
 			$this->pick('database.host', 'localhost', $host),
 			$this->pick('database.port', '3306', $port),
@@ -104,22 +133,22 @@ class Database
 			$this->pick('database.name', 'website', $name)
 		);
 	}
-	
+
 	protected function pick($key, $default, $override) {
 		if($override !== null)
 			return $override;
-			
+
 		return Config::get($key, $default);
 	}
-	
+
 	public function __isset($key) {
 		return isset($this->driver->{$key});
 	}
-	
+
 	public function __get($key) {
 		return $this->driver->{$key};
 	}
-	
+
 	public function __call($name, $args) {
 		return call_user_func_array(array($this->driver, $name), $args);
 	}
@@ -135,17 +164,17 @@ abstract class DB_Driver
 	protected $link;
 	protected $queryCount = 0;
 	public $error = '';
-	
+
 	public function __sleep()
 	{
 		return array('host', 'port', 'user', 'pass', 'name', 'error');
 	}
-	
+
 	public function __wakeup()
 	{
 		$this->onLoad();
 	}
-	
+
 	public function __construct($host, $port, $user, $pass, $name)
 	{
 		$this->host = $host;
@@ -155,55 +184,55 @@ abstract class DB_Driver
 		$this->name = $name;
 		$this->onLoad();
 	}
-	
+
 	public function __destruct()
 	{
 		$this->onUnload();
 	}
-	
+
 	public function getLink()
 	{
 		return $this->link;
 	}
-	
+
 	/* Securely establishes a connection to the database. */
 	public abstract function connect();
-	
+
 	/* Safely drops the connection to the database. */
 	public abstract function disconnect();
-	
+
 	/* Escapes a string so that it can be safely placed between single quotes in a query. */
 	public abstract function escape($str);
-	
+
 	/* Executes a database query. Returns <DB_Result>($driver, $affected, $insert_id, $text, $row, $success, $rows, $size, $time, $error) */
 	public abstract function query($query);
-	
+
 	/* Automatically called when the driver is unloaded. */
 	public abstract function onUnload();
-	
+
 	/* Automatically called when the driver is loaded. */
 	public abstract function onLoad();
-	
+
 	/* Create a prepared procedure. Returns <DB_PreparedStatement>($this, $statement) or false on failure. */
 	public abstract function prepare($statement);
-	
+
 	/* Execute a prepared procedure with parameters. Returns <DB_Result>($driver, $affected, $insert_id, $text, $row, $success, $rows, $size, $time, $error). */
 	public abstract function execute($statement, $params);
-	
+
 	/* Begins a transaction. Returns true if successful or throws DatabaseException on failure. */
 	public abstract function begin();
 	public function beginTransaction() { return $this->begin(); }
-	
+
 	/* Commit a transaction. Returns true on success or throws DatabaseException on failure. Sets back to autocommit mode. */
 	public abstract function commit();
-	
+
 	/* Rolls back the last transaction. Returns true on success or throws DatabaseException on failure. */
 	public abstract function rollback();
-	
+
 	/* Returns true if currently in a transaction and false otherwise. */
 	public abstract function inTransaction();
 
-	/* Performs the queries contained in the closure as a transaction. 
+	/* Performs the queries contained in the closure as a transaction.
 	   Throws an exception and rolls back changes if the transaction fails.
 	   Returns the value returned from the closure (if any). */
 	public /*mixed*/ function transaction(Closure $transaction) /*throws DatabaseException*/
@@ -226,14 +255,14 @@ class DB_PreparedStatement
 	protected $driver;
 	public $statement;
 	public $wrapper;
-	
+
 	public function __construct(&$driver, $statement, &$wrapper)
 	{
 		$this->driver =& $driver;
 		$this->statement = $statement;
 		$this->wrapper =& $wrapper;
 	}
-	
+
 	public function execute($opts=array())
 	{
 		if(!is_array($opts))
@@ -256,9 +285,9 @@ class DB_Result implements Iterator, ArrayAccess, Countable
 	public $driver;
 	public $time;
 	public $error;
-	
+
 	public function __construct(&$driver, $affected, $insertId, $text, $success, $rows, $size, $time, $error, $params=array()) {
-		
+
 		$this->affected = $affected;
 		$this->insertId = $insertId;
 		$this->text = $text;
@@ -269,55 +298,55 @@ class DB_Result implements Iterator, ArrayAccess, Countable
 		$this->time = $time;
 		$this->error = $error;
 		$this->params = $params;
-		
+
 		if($success === false && $driver->inTransaction() === false)
-			throw new DatabaseException("A database query failed: ".$this->text." : ".json_encode($params)." : ".$this->error);
+			throw new DatabaseException($this->text, $this->error, $this->params);
 	}
-	
+
 	public function __len()
 	{
 		return $this->size;
 	}
-	
+
 	public function count()
 	{
 		return $this->size;
 	}
-	
+
 	public function __get($key)
-	{	
+	{
 		if($key == 'row') {
 			if($this->size >= 1)
 				return $this->rows[0];
 			throw new RuntimeException("Access Violation: Access to 'row' failed; the query does not contain any data.");
 		}
-			
+
 		if(isset($this->row[$key]))
 			return $this->row[$key];
-		
+
 		throw new RuntimeException("Access Violation: Access to '".$key."' failed because it does not exist.");
 	}
-	
+
 	public function __invoke()
 	{
 		return $this->success;
 	}
-	
-	
-	
+
+
+
 	/*/ --
 	public function __call($name, $args)
 	{
 	}
-	
+
 	public function __toString()
 	{
 	}
-	
+
 	public function __set($name, $value)
 	{
 	}
-		
+
 	public function __isset($name)
 	{
 	}
@@ -326,41 +355,41 @@ class DB_Result implements Iterator, ArrayAccess, Countable
 	{
 	}
 	/*/
-		
+
 	// -- Iterator Methods
 	protected $__position = 0;
-	
+
 	public function rewind() {
 		$this->__position = 0;
 	}
-	
+
 	public function current() {
 		return $this->rows[$this->__position];
 	}
-	
+
 	public function key() {
 		return $this->__position;
 	}
-	
+
 	public function next() {
 		++$this->__position;
 	}
-	
+
 	public function valid() {
 		return isset($this->rows[$this->__position]);
 	}
-	
+
 	// -- ArrayAcess or ArrayObject Methods
 	public function offsetSet($offset, $value) {
 		throw new RuntimeException("Access Violation: You can not modify the result of a database query.");
 	}
-	
+
 	public function offsetExists($offset) {
 		if($offset === 0 || $offset === 1) // For backwards compatability.
 			return true;
 		return isset($this->rows[0][$offset]);
 	}
-	
+
 	public function offsetGet($offset) {
 		if($offset === 0) // For backwards compatability.
 			return $this->size;
@@ -370,7 +399,7 @@ class DB_Result implements Iterator, ArrayAccess, Countable
 			return $this->rows[0][$offset];
 		throw new RuntimeException("Access Violation: You attempted to access an undefined property '".$offset."'.");
 	}
-	
+
 	public function offsetUnset($offset) {
 		throw new RuntimeException("Access Violation: You can not modify the result of a database query.");
 	}
@@ -510,9 +539,9 @@ class DatabaseChunkIterator implements Iterator, ArrayAccess, Countable
 		if(substr($statement,-1) == ';')
 			$statement = substr($statement,0,-1);
 		$statement = $statement." LIMIT ".($offset * $this->chunkSize).",".$this->chunkSize.";";
-		
+
 		$query = $this->db->prepare($statement)->execute($this->parameters);
-		
+
 		if(len($query) > 0)
 			return $query->rows;
 		else
--- src/libraries/db-mysql.php
+++ src/libraries/db-mysql.php
@@ -11,19 +11,19 @@ class DB_Driver_mysql extends DB_Driver
 {
 	/* Whether or not we are in a transaction. */
 	protected $transaction = false;
-	
+
 	/* Whether or not the user can issue the rollback command. */
 	protected $allow_rollback = false;
-	
+
 	/* Whether or not we have established a connection to a database server. */
 	protected $connected = false;
-	
+
 	/* Securely establishes a connection to the database. */
 	public function connect()
 	{
 		if($this->connected)
 			return true;
-	
+
 		try {
 			$this->link = new PDO('mysql:host='.$this->host.';port='.$this->port.';dbname='.$this->name, $this->user, $this->pass);
 		}
@@ -31,28 +31,28 @@ class DB_Driver_mysql extends DB_Driver
 			trigger_error('Database Connection Failed: '.nl2br($e->getMessage()));
 			return false;
 		}
-		
+
 		$this->connected = true;
 		return true;
 	}
-	
+
 	/* Safely drops the connection to the database. */
 	public function disconnect()
 	{
 		if(!$this->connected)
 			return true;
-			
+
 		$this->link = null;
 		$this->connected = false;
 		return true;
 	}
-	
+
 	/* Escapes a string so that it can be safely placed between single quotes in a query. */
 	public function escape($str)
 	{
 		if(!$this->connected)
 			$this->connect();
-			
+
 		try {
 			$str = $this->link->quote($str);
 		}
@@ -60,54 +60,56 @@ class DB_Driver_mysql extends DB_Driver
 			trigger_error("Database Escape Failed: ".nl2br($e->getMessage()));
 			return false;
 		}
-		
+
 		$str = substr($str, 1, -1);
 		return $str;
 	}
-	
+
 	/* Executes a database query. Returns a <DB_Result>. */
 	public function query($query)
 	{
 		if(!$this->connected)
 			$this->connect();
-		
+
 		$this->queryCount++;
 		$timer = new DB_Timer();
-		
+
 		try {
 			$result = $this->link->query($query);
 			$insertId = $this->link->lastInsertId();
 		}
 		catch (PDOException $e) {
-			return new DB_Result($this, 0, null, $query, false, array(), 0, 0, $e->getMessage());
+			return new DB_Result($this, 0, 0, $query, false, array(), 0, 0, $e->getMessage(), []);
 		}
-		
-		if($result === false)
-			return new DB_Result($this, 0, null, $query, false, array(), 0, 0, implode(' | ',$this->link->errorInfo()));
-		
-		
+
+		if($result === false) {
+			return new DB_Result($this, 0, 0, $query, false, array(), 0, 0, implode(' | ',$this->link->errorInfo()), []);
+		}
+
+
 		$rows = $result->fetchAll(PDO::FETCH_ASSOC);
 		$row = isset($rows[0]) ? $rows[0] : array();
-		
+
 		return new DB_Result(
 			$this,
 			$result->rowCount(),
 			$insertId,
 			$query,
-			true,
+			true, // success
 			$rows,
 			len($rows),
-			$timer->reap(),
-			null
-		);		
+			$timer->reap(), // time
+			'', // error
+			[] // params
+		);
 	}
-	
+
 	/* Automatically called when the driver is unloaded. */
 	public function onUnload()
 	{
-		$this->disconnect();	
+		$this->disconnect();
 	}
-	
+
 	/* Automatically called when the driver is loaded. */
 	public function onLoad()
 	{
@@ -115,40 +117,39 @@ class DB_Driver_mysql extends DB_Driver
 			throw new Exception("You must enable the PDO extension to utilize database connections.");
 		}
 	}
-	
+
 	/* Create a prepared procedure. Returns <DB_PreparedStatement> or false on failure. */
 	public function prepare($statement)
 	{
 		if(!$this->connected)
 			$this->connect();
-			
+
 		try {
 			$res = $this->link->prepare($statement);
 		}
 		catch (PDOException $e) {
-			trigger_error('Database Prepare Failed: '.nl2br($e->getMessage()));
-			return false;
+			throw new DatabaseException('Database Prepare Failed: '.nl2br($e->getMessage()));
 		}
-		
+
 		if($res === false)
 			return false;
-		
+
 		return new DB_PreparedStatement(
 			$this,
 			$statement,
 			$res
 		);
 	}
-	
+
 	/* Execute a prepared procedure with parameters. Returns <DB_Result>. */
 	public function execute($statement, $params)
-	{		
+	{
 		if(!$this->connected)
 			$this->connect();
-			
+
 		$this->queryCount++;
 		$timer = new DB_Timer();
-			
+
 		try {
 			$result = $statement->wrapper->execute($params);
 			$insertId = $this->link->lastInsertId();
@@ -156,14 +157,14 @@ class DB_Driver_mysql extends DB_Driver
 		catch (PDOException $e) {
 			return new DB_Result($this, 0, null, $statement->statement, false, array(), 0, 0, $e->getMessage(), $params);
 		}
-		
+
 		if($result === false) {
-			return new DB_Result($this, 0, null, $statement->statement, false, array(), 0, 0, implode(' | ',$statement->wrapper->errorInfo()), $params);
+			return new DB_Result($this, 0, null, $statement->statement, false, array(), 0, 0, $statement->wrapper->errorInfo()[2], $params);
 		}
-			
+
 		$rows = $statement->wrapper->fetchAll(PDO::FETCH_ASSOC);
 		$row = isset($rows[0]) ? $rows[0] : array();
-				
+
 		return new DB_Result(
 			$this,
 			$statement->wrapper->rowCount(),
@@ -177,50 +178,50 @@ class DB_Driver_mysql extends DB_Driver
 			$params
 		);
 	}
-	
+
 	/* Begins a transaction. Returns true if successful or false on failure. */
 	public function begin()
 	{
 		if(!$this->connected)
 			$this->connect();
-			
+
 		try {
 			$res = $this->link->beginTransaction();
 		}
 		catch (PDOException $e) {
 			throw new DatabaseException('Transaction Start Failed: '.($e->getMessage()));
 		}
-		
+
 		if($res === true) {
 			$this->transaction = true;
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	/* Commit a transaction. Returns true on success or false on failure. Sets back to autocommit mode. */
 	public function commit()
 	{
 		if(!$this->transaction)
 			return;
-			
+
 		try {
 			$res = $this->link->commit();
 		}
 		catch (PDOException $e) {
 			throw new DatabaseException('Commit Failed: '.$e->getMessage());
 		}
-		
+
 		if($res === true) {
 			$this->transaction = false;
 			$this->allow_rollback = true;
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	/* Rolls back the last transaction. Returns true on success or false on failure. */
 	public function rollback()
 	{
@@ -228,7 +229,7 @@ class DB_Driver_mysql extends DB_Driver
 			return;
 		if(!$this->allow_rollback)
 			return;
-			
+
 		try {
 			$this->link->rollBack();
 		}
@@ -236,11 +237,11 @@ class DB_Driver_mysql extends DB_Driver
 			throw new DatabaseException("Rollback Failed: ".$e->getMessage());
 			return false;
 		}
-		
+
 		$this->allow_rollback = false;
-		return true;	
+		return true;
 	}
-	
+
 	/* Returns true if currently in a transaction and false otherwise. */
 	public function inTransaction()
 	{
